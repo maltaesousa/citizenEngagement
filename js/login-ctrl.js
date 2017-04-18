737ededc04898fe10108fe31f5a034a0a4011c66
InspecteurGadget <maltaesousa@gmail.com>
2017-04-18 19:29:52 +0200

    Added signup ctrl

--- js/login-ctrl.js
+++ js/login-ctrl.js
@@ -1,10 +1,11 @@
 angular.module('app').controller('LoginCtrl', function (AuthService, $http, $state) {
   var login = this;
   login.user = {};
-  login.password = {};
+  login.newuser = {};
+  login.checkPassword = "";
 
   login.connect = function () {
-    delete login.error;
+    delete login.alert;
     $http({
       method: 'POST',
       url: 'https://masrad-dfa-2017-c.herokuapp.com/api/auth',
@@ -13,8 +14,38 @@ angular.module('app').controller('LoginCtrl', function (AuthService, $http, $sta
       AuthService.setToken(res.data.token);
       $state.go('home');
     }).catch(function(error) {
-    login.error = "Unable to log you."
-    console.log(error);
-  });
+      login.alert = {
+          message : "Unable to log you.",
+          error: true
+        }
+
+      console.log(error);
+    });
+  };
+
+  login.createUser = function () {
+    delete login.alert;
+    if (login.newuser.password === login.checkPassword) {
+      login.newuser.roles = ["citizen"];
+      $http({
+        method: 'POST',
+        url: 'https://masrad-dfa-2017-c.herokuapp.com/api/users',
+        data: login.newuser
+      }).then(function (res) {
+        login.alert = {
+          message : "Your account has been created, please log in",
+          info: true
+        }
+        $state.go('login');
+      }).catch(function(error) {
+        login.alert = {
+          message : "Something went wrong",
+          error: true
+        }
+        console.log(error);
+      });
+    } else {
+      login.alert = "Passwords don't match";
+    }
   };
 });
