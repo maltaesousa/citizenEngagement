aee07417bd556afc4046b8c47fa68a96abe42327
InspecteurGadget <maltaesousa@gmail.com>
2017-04-25 18:40:11 +0200

    Added unique constraint on user name for signup

--- js/login-ctrl.js
+++ js/login-ctrl.js
@@ -2,7 +2,6 @@ angular.module('app').controller('LoginCtrl', function (AuthService, $http, $sta
   var login = this;
   login.user = {};
   login.newuser = {};
-  login.checkPassword = "";
 
   login.connect = function () {
     delete login.alert;
@@ -13,38 +12,40 @@ angular.module('app').controller('LoginCtrl', function (AuthService, $http, $sta
     }).then(function (res) {
       AuthService.setToken(res.data.token);
       $state.go('home');
-    }).catch(function(error) {
+    }).catch(function (error) {
       login.alert = {
-          message : "Unable to log you.",
-          error: true
-        }
-      console.log(error);
+        message: "Unable to log you.",
+        error: true
+      }
     });
   };
 
   login.createUser = function () {
     delete login.alert;
-    if (login.newuser.password === login.checkPassword) {
-      login.newuser.roles = ["citizen"];
-      $http({
-        method: 'POST',
-        url: 'https://masrad-dfa-2017-c.herokuapp.com/api/users',
-        data: login.newuser
-      }).then(function (res) {
+    login.newuser.roles = ["citizen"];
+    $http({
+      method: 'POST',
+      url: 'https://masrad-dfa-2017-c.herokuapp.com/api/users',
+      data: login.newuser
+    }).then(function (res) {
+      login.alert = {
+        message: "Your account has been created, please log in",
+        info: true
+      }
+      $state.go('login');
+    }).catch(function (error) {
+      if (error.data.name.kind === "user defined") {
         login.alert = {
-          message : "Your account has been created, please log in",
-          info: true
+          message: "Your username is already used"
         }
-        $state.go('login');
-      }).catch(function(error) {
-        login.alert = {
-          message : "Something went wrong",
-          error: true
+      } else {
+        login.alert ={
+          message : "FFFFUUUUUUU!! Internet is broken, come back later."
         }
-        console.log(error);
-      });
-    } else {
-      login.alert = "Passwords don't match";
-    }
+      }
+      login.alert.error = true;
+      console.log(error);
+      console.log(login.alert);
+    });
   };
 });
