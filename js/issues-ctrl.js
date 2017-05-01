25586e670feef6f82c89a1209d5a0f1d5ffb1abe
InspecteurGadget <maltaesousa@gmail.com>
2017-05-01 21:21:28 +0200

    Deleted unused import

--- js/issues-ctrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('app').controller('IssuesCtrl', function($scope, IssuesService) {
-  var issuesCtrl = this;
-
-  IssuesService.getIssues().then(function(issues) {
-    issuesCtrl.issues = issues;
-  });
-
-  $scope.$broadcast('my-event', {data: "ready"});
-});
